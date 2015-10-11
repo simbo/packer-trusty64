20bbaa07d0810deae8ccb18c62529fc47d39a3b5
Simon Lepel <simbo@simbo.de>
2015-10-11 14:12:15 +0200

    add some comments

--- trusty64/provision.sh
+++ trusty64/provision.sh
@@ -7,10 +7,12 @@ PROVISION_SCRIPTS="${PROVISION_DIR}/scripts"
 PROVISION_FILES="${PROVISION_DIR}/files"
 VAGRANT_HOME="/home/${VAGRANT_USER}"
 
+# highlight step messages
 function echo_c() {
     echo -e "\e[35m==> ${1}\e[0m"
 }
 
+# run provision steps
 source $PROVISION_SCRIPTS/apt.sh
 source $PROVISION_SCRIPTS/vagrant.sh
 source $PROVISION_SCRIPTS/vbox-guest-additions.sh
@@ -18,4 +20,5 @@ source $PROVISION_SCRIPTS/node.sh
 source $PROVISION_SCRIPTS/zsh.sh
 source $PROVISION_SCRIPTS/cleanup.sh
 
+# cleanup provition files
 rm -rf $PROVISION_DIR

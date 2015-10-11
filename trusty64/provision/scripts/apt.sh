97bdc86f357f5d055584127c7ed4f7f1cebbb6ae
Simon Lepel <simbo@simbo.de>
2015-10-11 14:12:07 +0200

    refactor package selection/installation; add linux-headers for guest additions

--- trusty64/provision/scripts/apt.sh
+++ trusty64/provision/scripts/apt.sh
@@ -5,21 +5,19 @@
 # ===================================
 ###
 
-APT_GET_QUIET="apt-get -y -qq"
-
 echo_c "Add custom sources..."
 
-$APT_GET_QUIET install software-properties-common
+apt-get -y -qq install software-properties-common
 add-apt-repository -y ppa:git-core/ppa
 
 echo_c "Updating apt sources..."
 
-$APT_GET_QUIET update
+apt-get -y -qq update
 
 echo_c "Upgrading packages..."
 
-$APT_GET_QUIET -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+apt-get -y -qq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
 echo_c "Install Packages..."
 
-$APT_GET_QUIET install curl vim mc htop iotop iftop tree nfs-common git zsh
+apt-get -y -qq install curl vim mc htop iotop iftop tree git zsh
